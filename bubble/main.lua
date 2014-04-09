b7f44943729fc1905b7592660961fd58f7511d0d
Vaskivo <maxteel1@gmail.com>
2014-04-09 01:11:17 +0100

    jdslf

--- bubble/main.lua
+++ bubble/main.lua
@@ -1,6 +1,6 @@
 Bubble = require 'src/bubble'
 
-MOAISim.openWindow ( "Cenas", 640, 480 )
+MOAISim.openWindow ( "Window", 640, 480 )
 
 viewport = MOAIViewport.new ()
 viewport:setSize ( 640, 480 )
@@ -20,11 +20,11 @@ layer:setBox2DWorld(world)
  
 bubble_list = {} 
  
-bubble_list[#bubble_list+1] = Bubble.new(0, 0, 25, world)
-bubble_list[#bubble_list+1] = Bubble.new(-50, 0, 25, world)
---bubble_list[#bubble_list+1] = Bubble.new(30, 30, 25, world)
---bubble_list[#bubble_list+1] = Bubble.new(0, -40, 35, world)
---bubble_list[#bubble_list+1] = Bubble.new(-30, -40, 10, world)
+bubble_list[#bubble_list+1] = Bubble.new(-200, 0, 25, 1, {1, 0, 0, 1}, world)
+bubble_list[#bubble_list+1] = Bubble.new(-100, -50, 25, 1, {1, 0, 0, 1}, world)
+--bubble_list[#bubble_list+1] = Bubble.new(30, 30, 25, 1, {1, 0, 0, 1}, world)
+--bubble_list[#bubble_list+1] = Bubble.new(0, -40, 35, 1, {1, 0, 0, 1}, world)
+--bubble_list[#bubble_list+1] = Bubble.new(-30, -40, 10, 1, {1, 0, 0, 1}, world)
 
 for _, y in pairs(bubble_list) do
     layer:insertProp(y.prop)
@@ -36,7 +36,8 @@ timer:setMode(MOAITimer.LOOP)
 timer:setListener(MOAITimer.EVENT_TIMER_LOOP, 
     function()    
         for _, y in pairs(bubble_list) do
-            y:increase_radius_by(1)
+            y:change_radius_by(1)
+            y:update_fixture()
         end
     end
     )
@@ -46,18 +47,35 @@ timer:start()
 --- walls
 left = world:addBody(MOAIBox2DBody.STATIC)
 left_fix = left:addRect(-320, -240, -300, 240)
+left_fix:setFriction(0)
     
 top = world:addBody(MOAIBox2DBody.STATIC)
 top_fix = top:addRect(-320, 220, 320, 240)
+top_fix:setFriction(0)    
     
 right = world:addBody(MOAIBox2DBody.STATIC)
 right_fix = right:addRect(300, -240, 320, 240)
+right_fix:setFriction(0)
 
 bottom = world:addBody(MOAIBox2DBody.STATIC)
 bottom_fix = bottom:addRect(-320, -240, 320, -220)
+bottom_fix:setFriction(0)
     
     
-    
+bubble1 = bubble_list[1]
+
+function main()
+    while true do
+        print('ITER')
+        print(bubble1.prop:getLoc())
+        print(bubble1.body:getPosition())
+        print('#####')
+        coroutine.yield()
+    end
+end
+
+thread = MOAICoroutine.new()
+--thread:run(main)
     
     
     
--- bubble/src/bubble.lua
+++ bubble/src/bubble.lua
@@ -3,78 +3,72 @@ local Bubble = {}
 Bubble.__index = Bubble
 
 
--- Constructor
+-- constructor
 
-function Bubble.new(x, y, radius, box2dWorld)
+function Bubble.new(x, y, starting_radius, growth_speed, color, box2d_world)
     local bubble = setmetatable({}, Bubble)
-    
-    bubble.radius = radius
-    
+
     local deck = MOAIScriptDeck.new()
     deck:setDrawCallback(function()
-            local x, y = bubble.body:getPosition()
-            MOAIGfxDevice.setPenColor(1, 0, 0, 1)
-            --MOAIDraw.fillCircle(x, y, bubble.radius, 64)
+            local radius = bubble.radius
+            --MOAIGfxDevice.setPenColor(unpack(color))
+            --MOAIDraw.fillCircle(0, 0, radius, 32)
+            --MOAIGfxDevice.setPenColor(1,1,1,1)
+            --MOAIGfxDevice.setPenWidth(3)
+            --MOAIDraw.drawCircle(0,0,radius,32)
+            -- I should use setRectCallback (but I don't know how!)
+            deck:setRect(-radius, -radius, radius, radius) 
         end
         )
-    deck:setRect(-25, -25, 25, 25)
-            
+    deck:setRect(-starting_radius, -starting_radius, starting_radius, starting_radius) 
+    
     local prop = MOAIProp2D.new()
     prop:setDeck(deck)
+    prop:setLoc(x, y)
     
-    local body = box2dWorld:addBody(MOAIBox2DBody.DYNAMIC, x, y)
-    --body:setTransform(x, y)
-    body:setMassData(50)
+    local body = box2d_world:addBody(MOAIBox2DBody.DYNAMIC)
+    body:setTransform(x, y)
     
-    prop:setAttrLink(MOAIProp2D.INHERIT_TRANSFORM, body, MOAIProp2D.TRANSFORM_TRAIT)
+    prop:setAttrLink (MOAITransform.ATTR_X_LOC, body, MOAITransform.ATTR_X_LOC)
+    prop:setAttrLink (MOAITransform.ATTR_Y_LOC, body, MOAITransform.ATTR_Y_LOC)
     
-    local fixture = body:addCircle(x, y, bubble.radius)
-    --[[
-    fixture:setCollisionHandler(
-        function (phase, fixtureA, fixtureB, arbiter)
-            print('CENAS')
-            local radius1 = fixtureA:getBody().data.radius
-            local radius2 = fixtureB:getBody().data.radius
-            
-            local x1, y1 = fixtureA:getBody():getPosition()
-            local x2, y2 = fixtureB:getBody():getPosition()
-            local v1 = x2-x1
-            local v2 = y2-y1
-            
-            local dist = math.sqrt(v1^2 + v2^2)
-            
-            if dist < (radius1 + radius2) then
-                print('GAME OVER!!')
-            end
-        end)
-    ]]--
-    
-    --fixture:setSensor(true)
+    local fixture = body:addCircle(0, 0, 50)
     fixture:setCollisionHandler(Bubble._collision_callback, MOAIBox2DArbiter.END)
     
+    fixture:setDensity(1)
+    fixture:setFriction(0)
+    body:resetMassData()
+    
+    bubble.start_radius = starting_radius
+    bubble.radius = starting_radius
+    bubble.growth_speed = growth_speed
+    bubble.color = color
     
     bubble.deck = deck
     bubble.prop = prop
     bubble.body = body
     bubble.fixture = fixture
     
-    bubble.body.data = bubble -- must be changed!!
+    bubble.body.data = bubble
+    
+    print(body:getWorldCenter())
+    
     return bubble
 end
 
+
+function Bubble.change_radius_by(self, value)
+    self.radius = self.radius + value
+end
+
+
 function Bubble.update_fixture(self)
     self.fixture:destroy()
-    local x, y = self.body:getPosition()
-    self.fixture = self.body:addCircle(x, y, self.radius)
-    --self.fixture:setSensor(true)
+    --local x, y = self.body:getPosition()
+    self.fixture = self.body:addCircle(0, 0, self.radius)
     self.fixture:setCollisionHandler(Bubble._collision_callback, MOAIBox2DArbiter.END)
 end
 
-
-function Bubble.increase_radius_by(self, inc)
-    self.radius = self.radius + inc
-    self:update_fixture()
-end
 
 function Bubble._collision_callback(phase, fixtureA, fixtureB, arbiter)
     if not fixtureB:getBody().data then -- colliding with outer wall
@@ -95,8 +89,6 @@ function Bubble._collision_callback(phase, fixtureA, fixtureB, arbiter)
         print('GAME OVER!!')
     end
 end
-
-
 
 
 
